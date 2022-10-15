bed931dedf9d468490dc1278cf8e9fc30a4b54aa
lghiggino <lghiggino@gmail.com>
2022-10-15 18:17:24 -0300

    3.3

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-const express = require("express")
-let people = require("./fixtures")
+const express = require("express");
+let people = require("./fixtures");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/persons', (request, response) => {
-    response.json(people)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(people);
+});
 
-app.get('/info', (request, response) => {
-    const responseHTML = `
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = people.find((person) => person.id === id);
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+});
+
+app.get("/info", (request, response) => {
+  const responseHTML = `
     <section>
         <p>Phonebook has info for ${people.length} people</p>
         <p>${new Date()}</p>
     </section>
-    `
-    response.send(responseHTML)
-})
+    `;
+  response.send(responseHTML);
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
