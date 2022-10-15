b20ae58fdc11ea9c808fb70d3fabc473b0013a37
lghiggino <lghiggino@gmail.com>
2022-10-15 18:03:10 -0300

    3.2

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.get('/api/persons', (request, response) => {
     response.json(people)
 })
 
+app.get('/info', (request, response) => {
+    const responseHTML = `
+    <section>
+        <p>Phonebook has info for ${people.length} people</p>
+        <p>${new Date()}</p>
+    </section>
+    `
+    response.send(responseHTML)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
