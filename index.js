33b62b540e8ac34d40e60848a0f416984e69a75b
lghiggino <lghiggino@gmail.com>
2022-10-15 17:29:39 -0300

    part3 persons first commit

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const app = require("./app")
-const http = require("http")
-const config = require("./utils/config")
-const logger = require("./utils/logger")
-
-const server = http.createServer(app)
-
-server.listen(config.PORT, () => {
-    logger.info(`Server running on ${config.PORT}`)
-})
+console.log('hello, hello')
